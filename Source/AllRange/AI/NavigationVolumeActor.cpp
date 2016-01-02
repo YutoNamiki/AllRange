958b4534ad084770145296d730f320207c4c9ea7
YutoNamiki <whiterunner.amanchu@gmail.com>
2016-01-02 19:31:21 +0900

    【AI】ナビゲーションボリュームの並列処理化処理途中

--- Source/AllRange/AI/NavigationVolumeActor.cpp
+++ Source/AllRange/AI/NavigationVolumeActor.cpp
@@ -8,7 +8,10 @@
 ANavigationVolumeActor::ANavigationVolumeActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
+
+	BoxVolume = CreateDefaultSubobject<UBoxComponent>(FName("Volume"));
+	RootComponent = BoxVolume;
 
 }
 
@@ -28,9 +31,75 @@ void ANavigationVolumeActor::Tick( float DeltaTime )
 
 void ANavigationVolumeActor::Initialize()
 {
+	WaypointList.Empty();
+	WaypointPathList.Empty();
+	DestroyChildrenComponents(BoxVolume);
+	TArray<TArray<FWaypoint>> createWaypointData;
+	createWaypointData.Init(TArray<FWaypoint>(), Recursion + 1);
+	DivideVolume(BoxVolume, DivideX, DivideY, DivideZ, createWaypointData[0]);
+
 }
 
-void ANavigationVolumeActor::DivideVolume(UBoxComponent * volume, int32 divX, int32 divY, int32 divZ)
+void ANavigationVolumeActor::DivideVolume(UBoxComponent* volume, int32 divX, int32 divY, int32 divZ, TArray<FWaypoint>& createData)
 {
+	const auto maxPosition = volume->GetUnscaledBoxExtent() * 1.0f;
+	const auto minPosition = volume->GetUnscaledBoxExtent() * -1.0f;
+	const auto divideScale = FVector(static_cast<float>(divX), static_cast<float>(divY), static_cast<float>(divZ));
+	const auto deltaVector = (maxPosition - minPosition) / divideScale;
+	auto num = 0;
+
+	for (auto x = minPosition.X; x < maxPosition.X; x += deltaVector.X)
+	{
+		for (auto y = minPosition.Y; y < maxPosition.Y; y += deltaVector.Y)
+		{
+			for (auto z = minPosition.Z; z < maxPosition.Z; z += deltaVector.Z)
+			{
+				const auto min = FVector(x, y, z);
+				const auto max = min + deltaVector;
+				createData.Add(CreateWaypoint(min, max, num++));
+			}
+		}
+	}
 }
 
+void ANavigationVolumeActor::DestroyChildrenComponents(USceneComponent * component)
+{
+	while (component->GetNumChildrenComponents() > 0)
+	{
+		const auto childComponent = component->GetChildComponent(0);
+		if (childComponent == nullptr)
+			continue;
+		DestroyChildrenComponents(childComponent);
+		if (childComponent->IsRegistered())
+			childComponent->UnregisterComponent();
+		childComponent->DestroyComponent(false);
+	}
+}
+
+FWaypoint ANavigationVolumeActor::CreateWaypoint(FVector minLocation, FVector maxLocation, int32 ID)
+{
+	return FWaypoint{ ID, EWaypointState::None, 0.0f, TArray<int32>(), -1, minLocation, maxLocation };
+}
+
+void ANavigationVolumeActor::CreateOctree(int32 recursionIndex, TArray<FWaypoint>& createData)
+{
+
+}
+
+CreateOctreeWorker::CreateOctreeWorker(FCriticalSection& mutex, TArray<TArray<FWaypoint>>& data, int32 recursionIndex)
+{
+	this->mutex = &mutex;
+	waypointData = &data;
+	this->recursionIndex = recursionIndex;
+}
+
+uint32 CreateOctreeWorker::Run()
+{
+
+	return 0;
+}
+
+void CreateOctreeWorker::Stop()
+{
+	stopTaskCounter.Increment();
+}
--- Source/AllRange/AI/NavigationVolumeActor.h
+++ Source/AllRange/AI/NavigationVolumeActor.h
@@ -3,7 +3,68 @@
 #pragma once
 
 #include "GameFramework/Actor.h"
+#include "HAL/ThreadingBase.h"
 #include "NavigationVolumeActor.generated.h"
+
+UENUM(BlueprintType)
+enum class EWaypointState : uint8
+{
+	None	UMETA(DisplayName = "None"),
+	Open	UMETA(DisplayName = "Open"),
+	Closed	UMETA(DisplayName = "Closed")
+};
+
+USTRUCT(BlueprintType)
+struct FWaypoint
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Waypoint")
+	int32 ID;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Waypoint")
+	EWaypointState State;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Waypoint")
+	float Cost;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Waypoint")
+	TArray<int32> NeighborWaypointIDs;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Waypoint")
+	int32 ParentWaypointID;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Waypoint")
+	FVector MinimumLocation;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Waypoint")
+	FVector MaximumLocation;
+
+};
+
+USTRUCT(BlueprintType)
+struct FWaypointPath
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "NavigationPath")
+	int32 ID1;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "NavigationPath")
+	int32 ID2;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "NavigationPath")
+	FColor DrawColor;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "NavigationPath")
+	float Thickness = 1.0f;
+
+};
+
+class CreateOctreeWorker : FRunnable
+{
+public:
+	CreateOctreeWorker(FCriticalSection& mutex, TArray<TArray<FWaypoint>>& data, int32 recursionIndex);
+	virtual uint32 Run() override;
+	virtual void Stop() override;
+
+private:
+	FThreadSafeCounter stopTaskCounter;
+	FCriticalSection* mutex;
+	TArray<TArray<FWaypoint>>* waypointData;
+	int32 recursionIndex;
+};
 
 UCLASS()
 class ALLRANGE_API ANavigationVolumeActor : public AActor
@@ -13,6 +74,10 @@ class ALLRANGE_API ANavigationVolumeActor : public AActor
 public:	
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "NavigationVolume")
 	UBoxComponent* BoxVolume;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Waypoint")
+	TArray<FWaypoint> WaypointList;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Waypoint")
+	TArray<FWaypointPath> WaypointPathList;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Waypoint")
 	int32 WaypointCount = 0;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Waypoint")
@@ -35,7 +100,10 @@ public:
 private:
 
 	void Initialize();
-	void DivideVolume(UBoxComponent* volume, int32 divX, int32 divY, int32 divZ);
-	
-	
+
+	static void DivideVolume(UBoxComponent* volume, int32 divX, int32 divY, int32 divZ, TArray<FWaypoint>& createData);
+	static void DestroyChildrenComponents(USceneComponent* component);
+	static FWaypoint CreateWaypoint(FVector minLocation, FVector maxLocation, int32 ID);
+	static void CreateOctree(int32 recursionIndex, TArray<FWaypoint>& createData);
+
 };
