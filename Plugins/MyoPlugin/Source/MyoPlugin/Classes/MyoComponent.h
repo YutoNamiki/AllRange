a9049ae04452906c526479be2192eecd56e67ed1
torihane <tobytobytbird@yahoo.co.jp>
2016-02-19 19:11:10 +0900

    【MyoPlugin】 MyoComponentを追加した際「+」を押すとイベントがブループリントに追加されるように記述

--- Plugins/MyoPlugin/Source/MyoPlugin/Classes/MyoComponent.h
+++ Plugins/MyoPlugin/Source/MyoPlugin/Classes/MyoComponent.h
@@ -3,6 +3,22 @@
 #include "MyoDelegateBlueprint.h"
 #include "MyoComponent.generated.h"
 
+class UMyoController;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMyoOnConnectDelegate, UMyoController*, Myo);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMyoOnDisconnectDelegate, UMyoController*, Myo);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMyoOnPairDelegate, UMyoController*, Myo);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMyoOnUnpairDelegate, UMyoController*, Myo);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FiveParams(FMyoOnArmMovedDelegate, UMyoController*, Myo, FVector, armAcceleration, FRotator, armOrientation, FVector, armGyro, MyoPose, pose);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FMyoOnOrientationDataDelegate, UMyoController*, myo, FRotator, orientation);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FMyoOnAccelerometerDataDelegate, UMyoController*, myo, FVector, acceleration);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FMyoOnGyroscopeDataDelegate, UMyoController*, myo, FVector, gyro);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FMyoOnPoseDelegate, UMyoController*, myo, MyoPose, pose);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FMyoOnArmSyncDelegate, UMyoController*, myo, MyoArm, arm, MyoArmDirection, direction);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMyoOnArmUnsyncDelegate, UMyoController*, Myo);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FMyoOnEmgDataDelegate, UMyoController*, myo, FMyoEmgData, data);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FMyoDeviceDisabledDelegate);
+
 UCLASS(Blueprintable, ClassGroup = "Input Controller", meta = (BlueprintSpawnableComponent))
 class MYOPLUGIN_API UMyoComponent : public UActorComponent, public IMyoDelegateBlueprint
 {
@@ -28,4 +44,45 @@ public:
 	void ConvertToMyoOrientationSpace(FRotator orientation, FRotator& converted);
 	UFUNCTION(BlueprintCallable, Category = MyoUtilityFunctions)
 	void VibrateDevice(int32 playerIndex, MyoVibrationType type);
+
+	virtual void OnConnect(int32 myoId) override;
+	virtual void OnDisconnect(int32 myoId) override;
+	virtual void OnPair(int32 myoId) override;
+	virtual void OnUnpair(int32 myoId) override;
+	virtual void OnArmMoved(int32 myoId, FVector armAcceleration, FRotator armOrientation, FVector armGyro, MyoPose pose);
+	virtual void OnOrientationData(int32 myoId, FRotator rot) override;
+	virtual void OnAccelerometerData(int32 myoId, FVector accel) override;
+	virtual void OnGyroscopeData(int32 myoId, FVector gyro) override;
+	virtual void OnPose(int32 myoId, MyoPose pose) override;
+	virtual void OnArmSync(int32 myoId, MyoArm arm, MyoArmDirection direction) override;
+	virtual void OnArmUnsync(int32 myoId) override;
+	virtual void OnEmgData(int32 myoId, FMyoEmgData data) override;
+	virtual void MyoDisabled() override;
+
+	UPROPERTY(BlueprintAssignable, Category = "Myo")
+	FMyoOnConnectDelegate MyoOnConnect;
+	UPROPERTY(BlueprintAssignable, Category = "Myo")
+	FMyoOnDisconnectDelegate MyoOnDisconnect;
+	UPROPERTY(BlueprintAssignable, Category = "Myo")
+	FMyoOnPairDelegate MyoOnPair;
+	UPROPERTY(BlueprintAssignable, Category = "Myo")
+	FMyoOnUnpairDelegate MyoOnUnpair;
+	UPROPERTY(BlueprintAssignable, Category = "Myo")
+	FMyoOnArmMovedDelegate MyoOnArmMoved;
+	UPROPERTY(BlueprintAssignable, Category = "Myo")
+	FMyoOnOrientationDataDelegate MyoOnOrientationData;
+	UPROPERTY(BlueprintAssignable, Category = "Myo")
+	FMyoOnAccelerometerDataDelegate MyoOnAccelerometerData;
+	UPROPERTY(BlueprintAssignable, Category = "Myo")
+	FMyoOnGyroscopeDataDelegate MyoOnGyroscopeData;
+	UPROPERTY(BlueprintAssignable, Category = "Myo")
+	FMyoOnPoseDelegate MyoOnPose;
+	UPROPERTY(BlueprintAssignable, Category = "Myo")
+	FMyoOnArmSyncDelegate MyoOnArmSync;
+	UPROPERTY(BlueprintAssignable, Category = "Myo")
+	FMyoOnArmUnsyncDelegate MyoOnArmUnsync;
+	UPROPERTY(BlueprintAssignable, Category = "Myo")
+	FMyoOnEmgDataDelegate MyoOnEmgData;
+	UPROPERTY(BlueprintAssignable, Category = "Myo")
+	FMyoDeviceDisabledDelegate MyoDeviceDisabled;
 };
--- Plugins/MyoPlugin/Source/MyoPlugin/Private/MyoComponent.cpp
+++ Plugins/MyoPlugin/Source/MyoPlugin/Private/MyoComponent.cpp
@@ -1,6 +1,6 @@
 #include "MyoPluginPrivatePCH.h"
 #include "MyoComponent.h"
-
+#include "MyoController.h"
 
 UMyoComponent::UMyoComponent(const FObjectInitializer& init) : Super(init)
 {
@@ -72,4 +72,83 @@ void UMyoComponent::VibrateDevice(int32 playerIndex, MyoVibrationType type)
 {
 	UE_LOG(MyoPluginLog, Warning, TEXT("UMyoComponent::VibrateDevice"));
 	MyoVibrateDevice(playerIndex, type);
+}
+
+void UMyoComponent::OnConnect(int32 myoId)
+{
+	//if (MyoIsValidId(myoId))
+	//	MyoOnConnect.Broadcast(Cast<UMyoController>(InterfaceDelegate));
+}
+void UMyoComponent::OnDisconnect(int32 myoId)
+{
+	//if (MyoIsValidId(myoId))
+	//	Execute_OnDisconnect(InterfaceDelegate, InternalControllerForId(myoId));
+}
+
+void UMyoComponent::OnPair(int32 myoId)
+{
+	if (myoId > LatestFrame.Num())
+		InternalAddController(myoId);
+	//if (MyoIsValidId(myoId))
+	//	Execute_OnPair(InterfaceDelegate, InternalControllerForId(myoId));
+}
+
+void UMyoComponent::OnUnpair(int32 myoId)
+{
+	//if (MyoIsValidId(myoId))
+	//	Execute_OnUnpair(InterfaceDelegate, InternalControllerForId(myoId));
+}
+
+void UMyoComponent::OnArmMoved(int32 myoId, FVector armAcceleration, FRotator armOrientation, FVector armGyro, MyoPose pose)
+{
+	//if (MyoIsValidId(myoId))
+	//	Execute_OnArmMoved(InterfaceDelegate, InternalControllerForId(myoId), armAcceleration, armOrientation, armGyro, pose);
+}
+
+void UMyoComponent::OnOrientationData(int32 myoId, FRotator rot)
+{
+	//if (MyoIsValidId(myoId))
+	//	Execute_OnOrientationData(InterfaceDelegate, InternalControllerForId(myoId), rot);
+}
+
+void UMyoComponent::OnAccelerometerData(int32 myoId, FVector accel)
+{
+	if (MyoIsValidId(myoId))
+		MyoOnAccelerometerData.Broadcast(Cast<UMyoController>(InterfaceDelegate), accel);
+}
+
+void UMyoComponent::OnGyroscopeData(int32 myoId, FVector gyro)
+{
+	//if (MyoIsValidId(myoId))
+	//	Execute_OnGyroscopeData(InterfaceDelegate, InternalControllerForId(myoId), gyro);
+}
+
+void UMyoComponent::OnPose(int32 myoId, MyoPose pose)
+{
+	//if (MyoIsValidId(myoId))
+	//	Execute_OnPose(InterfaceDelegate, InternalControllerForId(myoId), pose);
+}
+
+void UMyoComponent::OnArmSync(int32 myoId, MyoArm arm, MyoArmDirection direction)
+{
+	//if (MyoIsValidId(myoId))
+	//	Execute_OnArmSync(InterfaceDelegate, InternalControllerForId(myoId), arm, direction);
+}
+
+void UMyoComponent::OnArmUnsync(int32 myoId)
+{
+	//if (MyoIsValidId(myoId))
+	//	Execute_OnArmUnsync(InterfaceDelegate, InternalControllerForId(myoId));
+}
+
+void UMyoComponent::OnEmgData(int32 myoId, FMyoEmgData data)
+{
+	//if (MyoIsValidId(myoId))
+	//	Execute_OnEmgData(InterfaceDelegate, InternalControllerForId(myoId), data);
+}
+
+void UMyoComponent::MyoDisabled()
+{
+	//if (IsValidDelegate())
+	//	Execute_DeviceDisabled(InterfaceDelegate);
 }
